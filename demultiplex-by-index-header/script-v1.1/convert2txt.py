bb8c8862898b08ce312868b3fd1f88f423f9b6df
weichunjohnchenAH <weichun.chen@admerahealth.com>
2023-01-20 21:49:47 +0000

    edit-wc

---
+++ demultiplex-by-index-header/script-v1.1/convert2txt.py
@@ -0,0 +1,112 @@
+import csv
+import sys
+
+#### version 1.1
+###################################################################### extract information from samplesheet
+file=sys.argv[1]
+file=open(file, "r")
+data=csv.reader(file)
+
+samplesheet=[]
+for row in data:
+	samplesheet.append(row)
+#print(samplesheet)
+
+dat=[]
+for i in range(18, len(samplesheet), 1):
+	d=[]
+	samplesheet[i][6] = ':' + samplesheet[i][6]
+	samplesheet[i][8] = '+' + samplesheet[i][8]
+	d.append(samplesheet[i][1])
+	d.append(samplesheet[i][6])
+	d.append(samplesheet[i][8])
+	dat.append(d)
+#print(dat)
+
+mm1=[]
+mm2=[]
+for i in range(len(dat)):
+	x1=[]
+	x2=[]
+	for k in range(1,len(dat[0][1])):
+		index1=dat[i][1][:k] + '.' + dat[i][1][k+1:]
+		index2=dat[i][2][:k] + '.' + dat[i][2][k+1:]
+		x1.append(index1)
+		x2.append(index2)
+	mm1.append(x1)
+	mm2.append(x2)
+#print(mm1)
+#print(mm2)
+
+with open("temp.csv", "w", newline="") as f:
+	writer = csv.writer(f)
+	writer.writerows(dat)
+
+with open('mm1.txt', 'w') as m1:
+	for i in range(len(mm1)):
+		for j in range(len(mm1[0])):
+			if(j != len(mm1[0])-1):
+                        	m1.write(str(mm1[i][j]) + " ")
+			if(j == len(mm1[0])-1):
+				m1.write(str(mm1[i][j]) + '\n')
+
+with open('mm2.txt', 'w') as m2:
+        for i in range(len(mm2)):
+                for j in range(len(mm2[0])):
+                        if(j != len(mm2[0])-1):
+                                m2.write(str(mm2[i][j]) + " ")
+                        if(j == len(mm2[0])-1):
+                                m2.write(str(mm2[i][j]) + '\n')
+
+m1.close()
+m2.close()
+dat=[]
+mm1=[]
+wmm2=[]
+
+######################################################################
+text=open("temp.csv", "r")
+lines = text.readlines()
+a=[]
+for line in lines:
+        line = line.strip('\n')
+        line = line.replace(',',' ')
+        a.append(line)
+#print(a)
+
+#####################################################################
+with open('samplelist.txt', 'w') as s:
+	for i in range(0, len(a)):
+		s.write(str(a[i]) + '\n')
+s.close()
+
+
+'''
+###################################################################### divide barcodes into groups
+number=int(sys.argv[2])
+
+lst = open('listgroup.txt', 'w')
+
+num=[]
+for i in range(number, 20000, number):
+	num.append(i)
+
+for i in range(len(num)):
+	if len(a) >= num[i]:
+		if not len(a) <= num[i] and num[i]-len(a) < number:
+			lst.write(f'group{i}' + '\n')
+		with open(f'group{i}.txt', 'w') as s:
+
+			for j in range(number*i, len(a)):
+				if j < number*(i+1):
+					s.write(str(a[j]) + '\n')
+		s.close()
+	if len(a) <= num[i] and num[i]-len(a) < number:
+		lst.write(f'group{i}' + '\n')
+		with open(f'group{i}.txt', 'w') as s:
+			for j in range(number*i, len(a)):
+                                if j < number*(i+1):
+                                        s.write(str(a[j]) + '\n')
+		s.close()
+lst.close()
+'''
