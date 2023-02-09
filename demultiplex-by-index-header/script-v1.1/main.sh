6b9a31568515ad7c2ce7f8643c95999e07f87efa
weichunjohnchenAH <weichun.chen@admerahealth.com>
2023-02-09 03:57:48 +0000

    edit-wc

--- demultiplex-by-index-header/script-v1.1/main.sh
+++ demultiplex-by-index-header/script-v1.1/main.sh
@@ -7,6 +7,8 @@
 ################################################################################## calculate time
 start=$(date +%s%N)
 
+email="weichun.chen@admerahealth.com"
+
 ################################################################################## parse arguments
 while getopts w:h:p:j:a:b:s:l:m:k: flag
 do
@@ -24,20 +26,22 @@ k) type="${OPTARG}";;
 esac
 done
 
-#echo "Working directory: $workdir" # working directory where scripts are located
-#echo "nohup ID: $nohupid" # an abbreviated ID for the sh files displayed in jobs
-#echo "Parallel runs: $parallel_run # max number of parallels running at the same time
-#echo "Project folder name: $project_folder" # project folder name
-#echo "Read 1: $R1" # undetermined read 1
-#echo "Read 2: $R2" # undetermined read 2
-#echo "Samplesheet: $samplesheet"; # csv file
-#echo "Length of barcode: $barcode_length"; # number of nucleotides for barcode
-#echo "Number of mismatch: $mismatch" # 0 or 1
-#echo "Type of index: $type" # 1 = single or 2 = duel
+echo " "
+echo "Working directory: $workdir" # working directory where scripts are located
+echo "nohup ID: $nohupid" # an abbreviated ID for the sh files displayed in jobs
+echo "Parallel runs: $parallel_run" # max number of parallels running at the same time
+echo "Project folder name: $project_folder" # project folder name
+echo "Read 1: $R1" # undetermined read 1
+echo "Read 2: $R2" # undetermined read 2
+echo "Samplesheet: $samplesheet"; # csv file
+echo "Length of barcode: $barcode_length"; # number of nucleotides for barcode
+echo "Number of mismatch: $mismatch" # 0 or 1
+echo "Type of index: $type" # 1 = single or 2 = duel
+echo " "
 
 ############################################################################################################### function
 function can_run_new_nohup(){
-    #### To find how many specific nohup are running at the same time
+    ##### to find how many specific nohup are running at the same time
 
     local _set_p_run=$1
     local _nohup_id=$2
@@ -72,8 +76,8 @@ fi
 ############################################################################################################### extract information from samplesheet
 cd $workdir
 
-echo "Length of barcodes: $barcode_length"
-echo "Number of mismatch: $mismatch"
+#echo "Length of barcodes: $barcode_length"
+#echo "Number of mismatch: $mismatch"
 
 python3 convert2txt.py $samplesheet
 rm temp.csv
@@ -84,7 +88,7 @@ mv samplelist.txt $workdir/$project_folder/
 echo "Created a txt file containing variation of Index 1: mm1.txt"
 echo "Created a txt file containing variation of Index 2: mm2.txt"
 echo "Created a txt file containing sample and barcode list: samplelist.txt"
-echo "Moved mm1.txt, mm2.txt, and samplelist.txt to the project folder."
+echo "Moved mm1.txt, mm2.txt, and samplelist.txt to the project folder"
 
 ############################################################################################################### build array
 list=()
@@ -102,7 +106,7 @@ number=()
 for ((i=0; i<$len; i++)); do
 number+=("$i")
 done
-echo "${number[@]}"
+#echo "${number[@]}"
 
 
 mm1=() # i7 barcodes for mismatch
@@ -154,6 +158,12 @@ echo "Variation of Index 2: ${mm2[@]}"
 
 ############################################################################################################### start demultiplex
 ############################################################################################################### mismatch = 0
+echo " "
+echo " "
+echo "---------------------------------------------------------------------------"
+echo "start demultiplex..."
+echo " "
+
 if(($mismatch == 0)); then
 COUNT=0
 for ((i=0; i<${#number[@]}; i++)); do
@@ -179,8 +189,8 @@ nohup ./dmx0-R1.sh $R1 ${list[$COUNT + 1]} ${list[$COUNT + 2]} $workdir/$project
 
 nohup ./dmx0-R2.sh $R2 ${list[$COUNT + 1]} ${list[$COUNT + 2]} $workdir/$project_folder/fastq ${list[$COUNT]} 2 &
 echo ${list[$COUNT]}
-echo ${list[$COUNT + 1]}
-echo ${list[$COUNT + 2]}
+#echo ${list[$COUNT + 1]}
+#echo ${list[$COUNT + 2]}
 sleep 30s
 ps
 fi
@@ -190,8 +200,8 @@ nohup ./dmx0-R1.sh $R1 ${list[$COUNT + 1]} ${list[$COUNT + 2]} $workdir/$project
 
 nohup ./dmx0-R2.sh $R2 ${list[$COUNT + 1]} ${list[$COUNT + 2]} $workdir/$project_folder/fastq ${list[$COUNT]} 1 &
 echo ${list[$COUNT]}
-echo ${list[$COUNT + 1]}
-echo ${list[$COUNT + 2]}
+#echo ${list[$COUNT + 1]}
+#echo ${list[$COUNT + 2]}
 sleep 30s
 ps
 fi
@@ -204,8 +214,8 @@ nohup ./dmx0-R1.sh $R1 ${list[$COUNT + 1]} ${list[$COUNT + 2]} $workdir/$project
 
 nohup ./dmx0-R2.sh $R2 ${list[$COUNT + 1]} ${list[$COUNT + 2]} $workdir/$project_folder/fastq ${list[$COUNT]} 2 &
 echo ${list[$COUNT]}
-echo ${list[$COUNT + 1]}
-echo ${list[$COUNT + 2]}
+#echo ${list[$COUNT + 1]}
+#echo ${list[$COUNT + 2]}
 sleep 30s
 ps
 fi
@@ -215,8 +225,8 @@ nohup ./dmx0-R1.sh $R1 ${list[$COUNT + 1]} ${list[$COUNT + 2]} $workdir/$project
 
 nohup ./dmx0-R2.sh $R2 ${list[$COUNT + 1]} ${list[$COUNT + 2]} $workdir/$project_folder/fastq ${list[$COUNT]} 1 &
 echo ${list[$COUNT]}
-echo ${list[$COUNT + 1]}
-echo ${list[$COUNT + 2]}
+#echo ${list[$COUNT + 1]}
+#echo ${list[$COUNT + 2]}
 sleep 30s
 ps
 fi
@@ -229,8 +239,8 @@ nohup ./dmx0-R1.sh $R1 ${list[$COUNT + 1]} ${list[$COUNT + 2]} $workdir/$project
 
 nohup ./dmx0-R2.sh $R2 ${list[$COUNT + 1]} ${list[$COUNT + 2]} $workdir/$project_folder/fastq ${list[$COUNT]} 2 &
 echo ${list[$COUNT]}
-echo ${list[$COUNT + 1]}
-echo ${list[$COUNT + 2]}
+#echo ${list[$COUNT + 1]}
+#echo ${list[$COUNT + 2]}
 sleep 30s
 ps
 fi
@@ -240,8 +250,8 @@ nohup ./dmx0-R1.sh $R1 ${list[$COUNT + 1]} ${list[$COUNT + 2]} $workdir/$project
 
 nohup ./dmx0-R2.sh $R2 ${list[$COUNT + 1]} ${list[$COUNT + 2]} $workdir/$project_folder/fastq ${list[$COUNT]} 1 &
 echo ${list[$COUNT]}
-echo ${list[$COUNT + 1]}
-echo ${list[$COUNT + 2]}
+#echo ${list[$COUNT + 1]}
+#echo ${list[$COUNT + 2]}
 sleep 30s
 ps
 fi
@@ -279,10 +289,10 @@ nohup ./dmx6-R1.sh $R1 ${list[$COUNT + 1]} ${list[$COUNT + 2]} ${mm1[$COUNTER]} 
 
 nohup ./dmx6-R2.sh $R2 ${list[$COUNT + 1]} ${list[$COUNT + 2]} ${mm1[$COUNTER]} ${mm1[$COUNTER + 1]} ${mm1[$COUNTER + 2]} ${mm1[$COUNTER + 3]} ${mm1[$COUNTER + 4]} ${mm1[$COUNTER + 5]} ${mm2[$COUNTER]} ${mm2[$COUNTER + 1]} ${mm2[$COUNTER + 2]} ${mm2[$COUNTER + 3]} ${mm2[$COUNTER + 4]} ${mm2[$COUNTER + 5]} $workdir/$project_folder/fastq ${list[$COUNT]} 2 &
 echo ${list[$COUNT]}
-echo ${list[$COUNT + 1]}
-echo ${list[$COUNT + 2]}
-echo "Index 1: ${mm1[$COUNTER]}"
-echo "Index 2: ${mm2[$COUNTER]}"
+#echo ${list[$COUNT + 1]}
+#echo ${list[$COUNT + 2]}
+#echo "Index 1: ${mm1[$COUNTER]}"
+#echo "Index 2: ${mm2[$COUNTER]}"
 sleep 30s
 ps
 fi
@@ -292,8 +302,8 @@ nohup ./dmx6-R1.sh $R1 ${list[$COUNT + 1]} ${list[$COUNT + 2]} ${mm1[$COUNTER]} 
 
 nohup ./dmx6-R2.sh $R2 ${list[$COUNT + 1]} ${list[$COUNT + 2]} ${mm1[$COUNTER]} ${mm1[$COUNTER + 1]} ${mm1[$COUNTER + 2]} ${mm1[$COUNTER + 3]} ${mm1[$COUNTER + 4]} ${mm1[$COUNTER + 5]} ${mm2[$COUNTER]} ${mm2[$COUNTER + 1]} ${mm2[$COUNTER + 2]} ${mm2[$COUNTER + 3]} ${mm2[$COUNTER + 4]} ${mm2[$COUNTER + 5]} $workdir/$project_folder/fastq ${list[$COUNT]} 1 &
 echo ${list[$COUNT]}
-echo ${list[$COUNT + 1]}
-echo ${list[$COUNT + 2]}
+#echo ${list[$COUNT + 1]}
+#echo ${list[$COUNT + 2]}
 sleep 30s
 ps
 fi
@@ -306,10 +316,10 @@ nohup ./dmx8-R1.sh $R1 ${list[$COUNT + 1]} ${list[$COUNT + 2]} ${mm1[$COUNTER]} 
 
 nohup ./dmx8-R2.sh $R2 ${list[$COUNT + 1]} ${list[$COUNT + 2]} ${mm1[$COUNTER]} ${mm1[$COUNTER + 1]} ${mm1[$COUNTER + 2]} ${mm1[$COUNTER + 3]} ${mm1[$COUNTER + 4]} ${mm1[$COUNTER + 5]} ${mm1[$COUNTER + 6]} ${mm1[$COUNTER + 7]} ${mm2[$COUNTER]} ${mm2[$COUNTER + 1]} ${mm2[$COUNTER + 2]} ${mm2[$COUNTER + 3]} ${mm2[$COUNTER + 4]} ${mm2[$COUNTER + 5]} ${mm2[$COUNTER + 6]} ${mm2[$COUNTER + 7]} $workdir/$project_folder/fastq ${list[$COUNT]} 2 &
 echo ${list[$COUNT]}
-echo ${list[$COUNT + 1]}
-echo ${list[$COUNT + 2]}
-echo "Index 1: ${mm1[$COUNTER]}"
-echo "Index 2: ${mm2[$COUNTER]}"
+#echo ${list[$COUNT + 1]}
+#echo ${list[$COUNT + 2]}
+#echo "Index 1: ${mm1[$COUNTER]}"
+#echo "Index 2: ${mm2[$COUNTER]}"
 sleep 30s
 ps
 fi
@@ -319,8 +329,8 @@ nohup ./dmx8-R1.sh $R1 ${list[$COUNT + 1]} ${list[$COUNT + 2]} ${mm1[$COUNTER]} 
 
 nohup ./dmx8-R2.sh $R2 ${list[$COUNT + 1]} ${list[$COUNT + 2]} ${mm1[$COUNTER]} ${mm1[$COUNTER + 1]} ${mm1[$COUNTER + 2]} ${mm1[$COUNTER + 3]} ${mm1[$COUNTER + 4]} ${mm1[$COUNTER + 5]} ${mm1[$COUNTER + 6]} ${mm1[$COUNTER + 7]} ${mm2[$COUNTER]} ${mm2[$COUNTER + 1]} ${mm2[$COUNTER + 2]} ${mm2[$COUNTER + 3]} ${mm2[$COUNTER + 4]} ${mm2[$COUNTER + 5]} ${mm2[$COUNTER + 6]} ${mm2[$COUNTER + 7]} $workdir/$project_folder/fastq ${list[$COUNT]} 1 &
 echo ${list[$COUNT]}
-echo ${list[$COUNT + 1]}
-echo ${list[$COUNT + 2]}
+#echo ${list[$COUNT + 1]}
+#echo ${list[$COUNT + 2]}
 sleep 30s
 ps
 fi
@@ -333,10 +343,10 @@ nohup ./dmx10-R1.sh $R1 ${list[$COUNT + 1]} ${list[$COUNT + 2]} ${mm1[$COUNTER]}
 
 nohup ./dmx10-R2.sh $R2 ${list[$COUNT + 1]} ${list[$COUNT + 2]} ${mm1[$COUNTER]} ${mm1[$COUNTER + 1]} ${mm1[$COUNTER + 2]} ${mm1[$COUNTER + 3]} ${mm1[$COUNTER + 4]} ${mm1[$COUNTER + 5]} ${mm1[$COUNTER + 6]} ${mm1[$COUNTER + 7]} ${mm1[$COUNTER + 8]} ${mm1[$COUNTER + 9]} ${mm2[$COUNTER]} ${mm2[$COUNTER + 1]} ${mm2[$COUNTER + 2]} ${mm2[$COUNTER + 3]} ${mm2[$COUNTER + 4]} ${mm2[$COUNTER + 5]} ${mm2[$COUNTER + 6]} ${mm2[$COUNTER + 7]} ${mm2[$COUNTER + 8]} ${mm2[$COUNTER + 9]} $workdir/$project_folder/fastq ${list[$COUNT]} 2 &
 echo ${list[$COUNT]}
-echo ${list[$COUNT + 1]}
-echo ${list[$COUNT + 2]}
-echo "Index 1: ${mm1[$COUNTER]}"
-echo "Index 2: ${mm2[$COUNTER]}"
+#echo ${list[$COUNT + 1]}
+#echo ${list[$COUNT + 2]}
+#echo "Index 1: ${mm1[$COUNTER]}"
+#echo "Index 2: ${mm2[$COUNTER]}"
 sleep 30s
 ps
 fi
@@ -346,8 +356,8 @@ nohup ./dmx10-R1.sh $R1 ${list[$COUNT + 1]} ${list[$COUNT + 2]} ${mm1[$COUNTER]}
 
 nohup ./dmx10-R2.sh $R2 ${list[$COUNT + 1]} ${list[$COUNT + 2]} ${mm1[$COUNTER]} ${mm1[$COUNTER + 1]} ${mm1[$COUNTER + 2]} ${mm1[$COUNTER + 3]} ${mm1[$COUNTER + 4]} ${mm1[$COUNTER + 5]} ${mm1[$COUNTER + 6]} ${mm1[$COUNTER + 7]} ${mm1[$COUNTER + 8]} ${mm1[$COUNTER + 9]} ${mm2[$COUNTER]} ${mm2[$COUNTER + 1]} ${mm2[$COUNTER + 2]} ${mm2[$COUNTER + 3]} ${mm2[$COUNTER + 4]} ${mm2[$COUNTER + 5]} ${mm2[$COUNTER + 6]} ${mm2[$COUNTER + 7]} ${mm2[$COUNTER + 8]} ${mm2[$COUNTER + 9]} $workdir/$project_folder/fastq ${list[$COUNT]} 1 &
 echo ${list[$COUNT]}
-echo ${list[$COUNT + 1]}
-echo ${list[$COUNT + 2]}
+#echo ${list[$COUNT + 1]}
+#echo ${list[$COUNT + 2]}
 sleep 30s
 ps
 fi
@@ -371,16 +381,35 @@ break
 fi
 sleep 60s
 done
+echo " "
+echo "Demultiplex finished"
 
+echo " "
+echo " "
+echo "---------------------------------------------------------------------------"
 end=$(date +%s%N)
 echo "Elapsed Time (in seconds): $(($((end-$start))/1000000000)) seconds" > $workdir/$project_folder/runtime.txt
 echo "Elapsed Time (in seconds): $(($((end-$start))/1000000000)) seconds"
 echo "Elapsed Time (in minutes): $(($(($end-$start))/60000000000)) minutes" >> $workdir/$project_folder/runtime.txt
 echo "Elapsed Time (in minutes): $(($(($end-$start))/60000000000)) minutes"
 
-echo "Moved the log.out file to the project folder."
+echo " "
+echo " "
+echo "---------------------------------------------------------------------------"
+echo "Moved the log.out file to the project folder"
 mv ${project_folder}.log.out $workdir/$project_folder
 
-cd $workdir/$project
-chmod 777 -R $workdir/$project
+
+echo " "
+echo " "
+echo "---------------------------------------------------------------------------"
+echo "Set permission"
+cd $workdir/$project_folder
+chmod 777 -R $workdir/$project_folder
+
+echo " "
+echo " "
+echo "---------------------------------------------------------------------------"
+echo "Subject:Project ${project_folder}: Demultiplex Finished" | sendmail $email
+echo " "
 
